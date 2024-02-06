380465d9b2b10401bcef6c16b4166b2e8da68a55
AlexKochnov <kochnov.a@internet.ru>
2024-02-06 21:13:59 +0300

    Add msg classes: begining

--- UBXUnpacker.py
+++ UBXUnpacker.py
@@ -69,15 +69,27 @@ class Struct(object):
         return self.__dict__.items()
 
 
+class Message:
+    receiving_time: datetime = None
+    header: str = None
+
+    def __init__(self, receiving_time: datetime = None):
+        self.receiving_time = receiving_time or datetime.now()
+
+
+class Satellite:
+    def __init__(self, gnssId, svId):
+        self.gnssId = GNSS(gnssId)
+        self.svId = svId
+
+
 def flag_to_int(flags: bytes) -> int:
     # return sum([(flags[i] & 0xff) << 8 * (len(flags) - 1 - i) for i in range(len(flags))])
     return sum(flags[i] << 8 * i for i in range(len(flags)))
     pass
 
 
-def get_bytes_from_flag(flags: int, pattern: list[int] or int) -> int:
-    if pattern is int:
-        pattern = [pattern]
+def get_bytes_from_flag(flags: int, *pattern) -> int:
     return (flags >> min(pattern)) & sum(1 << (x - min(pattern)) for x in pattern)
 
 
@@ -158,51 +170,61 @@ def twosComp2dec(binaryStr: str) -> int:
     return intNumber
 
 
-def NAV_SAT(msg: bytes, receiving_time: datetime or None = None) -> (MessageTypes, list):
-    if receiving_time is None:
-        receiving_time = datetime.now()
+class NAV_SAT(Message):
     header = '<LBBBB'
-    iTOW, version, numsSvs, *reversed1 = struct.unpack(header, msg[:struct.calcsize(header)])
-    data = Struct(type='NAV_SAT', sys_time=receiving_time, iTOW=iTOW, version=version, numsSvs=numsSvs, SatNAV=[])
-    msg = msg[struct.calcsize(header):]
+    SAT = dict()
 
-    for i in range(numsSvs):
-        gnssId, svId, cno, elev, azim, prRes, flags = struct.unpack('<BBBbhh4s', msg[12 * i: 12 * (i + 1)])
-        flags = flag_to_int(flags)
-        bitfield = lambda *args: get_bytes_from_flag(flags, args)
-        SatData = Struct(gnssId=gnssId, svId=svId, cno=cno, elev=elev, azim=azim, prRes=prRes / 10.0,
-                         qualityInd=bitfield(0, 1, 2),
-                         svUsed=bitfield(3),
-                         health=bitfield(4, 5),
-                         ephAvail=bitfield(11),
-                         almAvail=bitfield(12))
-        data.SatNAV.append(SatData)
+    class SV_SAT:
+        def __init__(self, cno, elev, azim, prRes, flags):
+            self.cno = cno
+            self.elev = elev
+            self.azim = azim
+            self.prRes = prRes
+            int_flags = flag_to_int(flags)
+            self.flags = {
+                'ualityInd': get_bytes_from_flag(int_flags, 0, 1, 2),
+                'svUsed': get_bytes_from_flag(int_flags, 3),
+                'health': get_bytes_from_flag(int_flags, 4, 5),
+                'ephAvail': get_bytes_from_flag(int_flags, 11),
+                'almAvail': get_bytes_from_flag(int_flags, 12)
+            }
 
-    return MessageTypes.NAV, data
+    def __init__(self, msg: bytes, receiving_time: datetime = None):
+        super().__init__(receiving_time)
+        self.iTOW, version, numsSvs, *reversed1 = struct.unpack(self.header, msg[:struct.calcsize(self.header)])
+        msg = msg[struct.calcsize(self.header):]
+        for i in range(numsSvs):
+            gnssId, svId, cno, elev, azim, prRes, flags = struct.unpack('<BBBbhh4s', msg[12 * i: 12 * (i + 1)])
+            self.SAT[Satellite(gnssId, svId)] = self.SV_SAT(cno, elev, azim, prRes, flags)
 
 
-def NAV_ORB(msg: bytes, receiving_time: datetime or None = None) -> (MessageTypes, list):
-    if receiving_time is None:
-        receiving_time = datetime.now()
+class NAV_ORB(Message):
     header = '<LBBBB'
-    iTOW, version, numsSvs, *reversed1 = struct.unpack(header, msg[:struct.calcsize(header)])
-    data = Struct(type='NAV_ORB', sys_time=receiving_time, iTOW=iTOW, version=version, numsSvs=numsSvs, SatORB=[])
-    msg = msg[struct.calcsize(header):]
+    ORB = dict()
 
-    for i in range(numsSvs):
-        gnssId, svId, svFlag, eph, alm, otherOrb = struct.unpack('<BBssss', msg[6 * i: 6 * (i + 1)])
-        svFlag = flag_to_int(svFlag)
-        eph = flag_to_int(eph)
-        alm = flag_to_int(alm)
-        otherOrb = flag_to_int(otherOrb)
-        SatData = Struct(gnssId=gnssId, svId=svId, health=svFlag % 4, visibility=(svFlag // 4) % 4,
-                         ephUsability=eph % 32, ephSource=eph // 32,
-                         almUsability=alm % 32, almSource=alm // 32,
-                         anoAopUsability=otherOrb % 32, type=otherOrb // 32
-                         )
-        data.SatORB.append(SatData)
+    class SV_ORB:
+        def __init__(self, svFlag, eph, alm, otherOrb):
+            svFlag = flag_to_int(svFlag)
+            eph = flag_to_int(eph)
+            alm = flag_to_int(alm)
+            otherOrb = flag_to_int(otherOrb)
 
-    return MessageTypes.NAV, data
+            self.health = svFlag % 4
+            self.visibility = (svFlag // 4) % 4,
+            self.ephUsability = eph % 32
+            self.ephSource = eph // 32,
+            self.almUsability = alm % 32
+            self.almSource = alm // 32,
+            self.anoAopUsability = otherOrb % 32
+            self.type = otherOrb // 32
+
+    def __init__(self, msg: bytes, receiving_time: datetime = None):
+        super().__init__(receiving_time)
+        self.iTOW, version, numsSvs, *reversed1 = struct.unpack(self.header, msg[:struct.calcsize(self.header)])
+        msg = msg[struct.calcsize(self.header):]
+        for i in range(numsSvs):
+            gnssId, svId, svFlag, eph, alm, otherOrb = struct.unpack('<BBssss', msg[6 * i: 6 * (i + 1)])
+            self.ORB[Satellite(gnssId, svId)] = self.SV_ORB(svFlag, eph, alm, otherOrb)
 
 
 def NAV_TIMEGPS(msg: bytes, receiving_time: datetime or None = None) -> (MessageTypes, list):
@@ -307,66 +329,81 @@ def ParseSfGPS(msg: bytes, numWords):
     return res
 
 
-def ParseSf1(subframe):
-    week = bin2dec(subframe[60:70]) + 0 * 1024 * 2
-    accuracy = bin2dec(subframe[72:76])
-    health = bin2dec(subframe[76:82])
-    IODC = bin2dec(subframe[82:84] + subframe[196:204])
-    Tgd = twosComp2dec(subframe[195:204]) * 2 ** (- 31)
-    Toc = bin2dec(subframe[218:234]) * 2 ** 4
-    af2 = twosComp2dec(subframe[240:248]) * 2 ** (- 55)
-    af1 = twosComp2dec(subframe[248:264]) * 2 ** (- 43)
-    af0 = twosComp2dec(subframe[270:292]) * 2 ** (- 31)
-    return [week, accuracy, health, IODC, Tgd, Toc, af2, af1, af0]
+class GPSParser:
 
+    def parse(self, subframe, subframe_number: int) -> list:
+        return {
+            1: self.ParseSf1,
+            2: self.ParseSf2,
+            3: self.ParseSf3,
+            5: self.ParseSf5,
+        }[subframe_number](subframe)
 
-def ParseSf2(subframe):
-    IODE1 = bin2dec(subframe[60:68])
-    Crs = twosComp2dec(subframe[68:84]) * 2 ** (- 5)
-    dn = twosComp2dec(subframe[90:106]) * 2 ** (- 43)
-    M0 = twosComp2dec(subframe[106:114] + subframe[120:144]) * 2 ** (- 31)
-    Cuc = twosComp2dec(subframe[150:166]) * 2 ** (- 29)
-    e = bin2dec(subframe[166:174] + subframe[180:204]) * 2 ** (- 33)
-    Cus = twosComp2dec(subframe[210:226]) * 2 ** (- 29)
-    sqrtA = bin2dec(subframe[226:234] + subframe[240:264]) * 2 ** (- 19)
-    Toe = bin2dec(subframe[270:286]) * 2 ** 4
-    return [IODE1, Crs, dn, M0, Cuc, e, Cus, sqrtA, Toe]
+    class Sf1:
+        __slots__ = ['week', 'accuracy', 'health', 'IODC', 'Tgd', 'Toc', 'af2', 'af1', 'af0']
 
+    @staticmethod
+    def ParseSf1(subframe):
+        week = bin2dec(subframe[60:70]) + 0 * 1024 * 2
+        accuracy = bin2dec(subframe[72:76])
+        health = bin2dec(subframe[76:82])
+        IODC = bin2dec(subframe[82:84] + subframe[196:204])
+        Tgd = twosComp2dec(subframe[195:204]) * 2 ** (- 31)
+        Toc = bin2dec(subframe[218:234]) * 2 ** 4
+        af2 = twosComp2dec(subframe[240:248]) * 2 ** (- 55)
+        af1 = twosComp2dec(subframe[248:264]) * 2 ** (- 43)
+        af0 = twosComp2dec(subframe[270:292]) * 2 ** (- 31)
+        return [week, accuracy, health, IODC, Tgd, Toc, af2, af1, af0]
 
-def ParseSf3(subframe):
-    Cic = twosComp2dec(subframe[60:76]) * 2 ** (- 29)
-    W0 = twosComp2dec(subframe[76:84] + subframe[90:114]) * 2 ** (- 31)
-    Cis = twosComp2dec(subframe[120:136]) * 2 ** (- 29)
-    i0 = twosComp2dec(subframe[136:144] + subframe[150:174]) * 2 ** (- 31)
-    Crc = twosComp2dec(subframe[180:196]) * 2 ** (- 5)
-    w = twosComp2dec(subframe[196:204] + subframe[210:234]) * 2 ** (- 31)
-    Wdot = twosComp2dec(subframe[240:264]) * 2 ** (- 43)
-    IODE2 = bin2dec(subframe[270:278])
-    IDOT = twosComp2dec(subframe[278:292]) * 2 ** (- 43)
-    return [Cic, W0, Cis, i0, Crc, w, Wdot, IODE2, IDOT]
+    @staticmethod
+    def ParseSf2(subframe):
+        IODE1 = bin2dec(subframe[60:68])
+        Crs = twosComp2dec(subframe[68:84]) * 2 ** (- 5)
+        dn = twosComp2dec(subframe[90:106]) * 2 ** (- 43)
+        M0 = twosComp2dec(subframe[106:114] + subframe[120:144]) * 2 ** (- 31)
+        Cuc = twosComp2dec(subframe[150:166]) * 2 ** (- 29)
+        e = bin2dec(subframe[166:174] + subframe[180:204]) * 2 ** (- 33)
+        Cus = twosComp2dec(subframe[210:226]) * 2 ** (- 29)
+        sqrtA = bin2dec(subframe[226:234] + subframe[240:264]) * 2 ** (- 19)
+        Toe = bin2dec(subframe[270:286]) * 2 ** 4
+        return [IODE1, Crs, dn, M0, Cuc, e, Cus, sqrtA, Toe]
 
-def ParseSf5(subframe):
-    Data_ID = bin2dec(subframe[60:62])
-    SV_ID = bin2dec(subframe[62:68])
-    e = bin2dec(subframe[68:84]) * 2 ** (-21)
-    Toa = bin2dec(subframe[90:98]) * 2 ** 12
-    delta_i = twosComp2dec(subframe[98:114]) * 2 ** (-19)
-    Wdot = twosComp2dec(subframe[120:136]) * 2 ** (-38)
-    health = bin2dec(subframe[136:144])
-    sqrtA = bin2dec(subframe[150:174]) * 2 ** (-11)
-    W0 = twosComp2dec(subframe[180:204]) * 2 ** (-23)
-    w = twosComp2dec(subframe[210:234]) * 2 ** (-23)
-    M0 = twosComp2dec(subframe[240:264]) * 2 ** (-23)
-    af1 = twosComp2dec(subframe[278:289]) * 2 ** (-38)
-    af0 = twosComp2dec(subframe[270:278] + subframe[289:292]) * 2 ** (-20)
+    @staticmethod
+    def ParseSf3(subframe):
+        Cic = twosComp2dec(subframe[60:76]) * 2 ** (- 29)
+        W0 = twosComp2dec(subframe[76:84] + subframe[90:114]) * 2 ** (- 31)
+        Cis = twosComp2dec(subframe[120:136]) * 2 ** (- 29)
+        i0 = twosComp2dec(subframe[136:144] + subframe[150:174]) * 2 ** (- 31)
+        Crc = twosComp2dec(subframe[180:196]) * 2 ** (- 5)
+        w = twosComp2dec(subframe[196:204] + subframe[210:234]) * 2 ** (- 31)
+        Wdot = twosComp2dec(subframe[240:264]) * 2 ** (- 43)
+        IODE2 = bin2dec(subframe[270:278])
+        IDOT = twosComp2dec(subframe[278:292]) * 2 ** (- 43)
+        return [Cic, W0, Cis, i0, Crc, w, Wdot, IODE2, IDOT]
 
-    return [SV_ID, Data_ID, Toa, e, delta_i, Wdot, sqrtA, W0, w, M0, af0, af1, health]
+    @staticmethod
+    def ParseSf5(subframe):
+        Data_ID = bin2dec(subframe[60:62])
+        SV_ID = bin2dec(subframe[62:68])
+        e = bin2dec(subframe[68:84]) * 2 ** (-21)
+        Toa = bin2dec(subframe[90:98]) * 2 ** 12
+        delta_i = twosComp2dec(subframe[98:114]) * 2 ** (-19)
+        Wdot = twosComp2dec(subframe[120:136]) * 2 ** (-38)
+        health = bin2dec(subframe[136:144])
+        sqrtA = bin2dec(subframe[150:174]) * 2 ** (-11)
+        W0 = twosComp2dec(subframe[180:204]) * 2 ** (-23)
+        w = twosComp2dec(subframe[210:234]) * 2 ** (-23)
+        M0 = twosComp2dec(subframe[240:264]) * 2 ** (-23)
+        af1 = twosComp2dec(subframe[278:289]) * 2 ** (-38)
+        af0 = twosComp2dec(subframe[270:278] + subframe[289:292]) * 2 ** (-20)
+        return [SV_ID, Data_ID, Toa, e, delta_i, Wdot, sqrtA, W0, w, M0, af0, af1, health]
+
 
 def RXM_SFRBX(msg: bytes, receiving_time: datetime or None = None) -> dict[str: object]:
     if receiving_time is None:
         receiving_time = datetime.now()
     header = '<BBBBBBBB'
-    gnssID, svId, _, freqId, numWords, chn, version,_  = struct.unpack(header, msg[:struct.calcsize(header)])
+    gnssID, svId, _, freqId, numWords, chn, version, _ = struct.unpack(header, msg[:struct.calcsize(header)])
     if version == 0x02:
         chn = None
     msg = msg[struct.calcsize(header):]
@@ -378,29 +415,29 @@ def RXM_SFRBX(msg: bytes, receiving_time: datetime or None = None) -> dict[str: 
         # match sfId:
         #     case 1:
         # [week, accuracy, health, IODC, Tgd, Toc, af2, af1, af0]\
-        data = ParseSf1(subframe)
+        data = GPSParser.parse(subframe, 1)
         h = ['week', 'accuracy', 'health', 'IODC', 'Tgd', 'Toc', 'af2', 'af1', 'af0']
         print(tabulate([data], headers=h))
         print('\n')
-            # case 2:
+        # case 2:
         # [IODE1, Crs, dn, M0, Cuc, e, Cus, sqrtA, Toe] \
-        data = ParseSf2(subframe)
+        data = GPSParser.parse(subframe, 2)
         h = ['IODE1', 'Crs', 'dn', 'M0', 'Cuc', 'e', 'Cus', 'sqrtA', 'Toe']
         print(tabulate([data], headers=h))
         print('\n')
-            # case 3:
+        # case 3:
         # [Cic, W0, Cis, i0, Crc, w, Wdot, IODE2, IDOT]
-        data = ParseSf3(subframe)
+        data = GPSParser.parse(subframe, 3)
         h = ['Cic', 'W0', 'Cis', 'i0', 'Crc', 'w', 'Wdot', 'IODE2', 'IDOT']
         print(tabulate([data], headers=h))
         print('\n')
-            # case 5:
+        # case 5:
         h = ['SV_ID', 'Data_ID', 'Toa', 'e', 'delta_i', 'Wdot', 'sqrtA', 'W0', 'w', 'M0', 'af0', 'af1', 'health']
-        data = ParseSf5(subframe)
+        data = GPSParser.parse(subframe, 5)
         print(tabulate([data], headers=h))
         print('\n')
-            # case _:
-            #     data = None
+        # case _:
+        #     data = None
         # if data:
         #     print(tabulate(data, headers=h))
     print(f'SFRBX : {gnssID} : {svId} : {sfId} : {subframe}')
@@ -434,7 +471,7 @@ def NAV_VELECEF(msg: bytes, receiving_time: datetime or None = None) -> dict[str
     if receiving_time is None:
         receiving_time = datetime.now()
     iTOW, ecefVX, ecefVY, ecefVZ, tAcc = struct.unpack('<LlllL', msg)
-    data = Struct(type='NAV_POSECEF', sys_time=receiving_time, iTOW=iTOW, tAcc_ecefV=tAcc / 100,
+    data = Struct(type='NAV_VELECEF', sys_time=receiving_time, iTOW=iTOW, tAcc_ecefV=tAcc / 100,
                   ecefVX=ecefVX / 100.0, ecefVY=ecefVY / 100.0, ecefVZ=ecefVZ / 100.0)
     data.ECEFV = (data.ecefVX, data.ecefVY, data.ecefVZ)
     return MessageTypes.NAV, data
