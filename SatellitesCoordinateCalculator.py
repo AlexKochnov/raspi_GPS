b929316052f4cd286b5cd4b7331928e597c83253
AlexKochnov <kochnov.a@internet.ru>
2024-07-29 17:11:42 +0300

    Transport coordination Calculator from ipynb into code

--- SatellitesCoordinateCalculator.py
+++ SatellitesCoordinateCalculator.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from math import pi, floor, sin, cos, atan, sqrt, tan, atan2
 from scipy.integrate import RK45 , solve_ivp, odeint
 from scipy.integrate._ivp.ivp import OdeResult
@@ -6,6 +7,7 @@ import numpy as np
 import pandas as pd
 
 import Constants
+from GNSS import GNSS
 
 
 def check_gps_time(time):
@@ -347,3 +349,129 @@ def calc_glo_alm(alm, N4, NA, t1, N, hard=True):
     # Vz = Vr * (sin(u)*sin(i)) + Vu * (cos(u)*sin(i))
 
     return X, Y, Z
+
+
+class RK45Solver:
+    order = 5
+    error_estimator_order = 4
+    n_stages = 6
+    C = np.array([0, 1 / 5, 3 / 10, 4 / 5, 8 / 9, 1])
+    A = np.array([
+        [0, 0, 0, 0, 0],
+        [1 / 5, 0, 0, 0, 0],
+        [3 / 40, 9 / 40, 0, 0, 0],
+        [44 / 45, -56 / 15, 32 / 9, 0, 0],
+        [19372 / 6561, -25360 / 2187, 64448 / 6561, -212 / 729, 0],
+        [9017 / 3168, -355 / 33, 46732 / 5247, 49 / 176, -5103 / 18656]
+    ])
+    B = np.array([35 / 384, 0, 500 / 1113, 125 / 192, -2187 / 6784, 11 / 84])
+    E = np.array([-71 / 57600, 0, 71 / 16695, -71 / 1920, 17253 / 339200, -22 / 525,
+                  1 / 40])
+    P = np.array([
+        [1, -8048581381 / 2820520608, 8663915743 / 2820520608,
+         -12715105075 / 11282082432],
+        [0, 0, 0, 0],
+        [0, 131558114200 / 32700410799, -68118460800 / 10900136933,
+         87487479700 / 32700410799],
+        [0, -1754552775 / 470086768, 14199869525 / 1410260304,
+         -10690763975 / 1880347072],
+        [0, 127303824393 / 49829197408, -318862633887 / 49829197408,
+         701980252875 / 199316789632],
+        [0, -282668133 / 205662961, 2019193451 / 616988883, -1453857185 / 822651844],
+        [0, 40617522 / 29380423, -110615467 / 29380423, 69997945 / 29380423]])
+
+    def __init__(self, fun, y0, t0, step=1):
+        self.fun = fun
+        self.t = [t0]
+        self.y = [y0]
+        self.f = fun(t0, y0)
+        self.step = step
+        self.n = y0.size
+        self.K = np.empty((self.n_stages + 1, self.n), dtype=y0.dtype)
+
+    def get(self, t):
+        if t < self.t[0]:
+            return None
+        while t > self.t[-1]:
+            self.next()
+        return self.y[-1]
+
+    def next(self):
+        self.t.append(self.t[-1] + self.step)
+        y, self.f = self.rk_step(self.fun, self.t[-1], self.y[-1], self.f, self.step, self.A, self.B, self.C, self.K)
+        self.y.append(y)
+        return self.t[-1], self.y[-1]
+
+    @staticmethod
+    def rk_step(fun, t, y, f, h, A, B, C, K):
+        K[0] = f
+        for s, (a, c) in enumerate(zip(A[1:], C[1:]), start=1):
+            dy = np.dot(K[:s].T, a[:s]) * h
+            K[s] = fun(t + c * h, y + dy)
+        y_new = y + h * np.dot(K[:-1].T, B)
+        f_new = fun(t + h, y_new)
+        K[-1] = f_new
+        return y_new, f_new
+
+
+def get_glo_eph_simple_func(eph):
+    X0 = eph.x
+    Vx0 = eph.dx
+    ddx = eph.ddx
+    Y0 = eph.y
+    Vy0 = eph.dy
+    ddy = eph.ddy
+    Z0 = eph.z
+    Vz0 = eph.dz
+    ddz = eph.ddz
+    tb = eph.tb
+
+    maxT = lambda ti: ti - round((ti - tb) / 86400) * 86400
+
+    ae = Constants.ae_glonass
+    GM = Constants.mu
+    J20 = Constants.J20_glonass
+    OmegaEarth = Constants.OmegaEarthDot
+
+    y0 = np.array([X0, Y0, Z0, Vx0, Vy0, Vz0]) * 1000
+
+    def func(t, y):
+        X, Y, Z, Vx, Vy, Vz = y
+        r = sqrt(X ** 2 + Y ** 2 + Z ** 2)
+        dVx = -GM / r ** 3 * X - 1.5 * J20 * GM * ae ** 2 / r ** 5 * X * (
+                    1 - 5 * Z ** 2 / r ** 2) + OmegaEarth ** 2 * X + 2 * OmegaEarth * Vy + ddx
+        dVy = -GM / r ** 3 * Y - 1.5 * J20 * GM * ae ** 2 / r ** 5 * Y * (
+                    1 - 5 * Z ** 2 / r ** 2) + OmegaEarth ** 2 * Y - 2 * OmegaEarth * Vx + ddy
+        dVz = -GM / r ** 3 * Z - 1.5 * J20 * GM * ae ** 2 / r ** 5 * Z * (3 - 5 * Z ** 2 / r ** 2) + ddz
+        # print(t, np.array([X, Y, Z, Vx, Vy, Vz, dVx, dVy, dVz]).round(0))
+        return np.array([Vx, Vy, Vz, dVx, dVy, dVz])
+
+    return func, y0, eph.tb
+
+
+class CalcMode(Enum):
+    alm = 0
+    eph = 1
+    pass
+
+
+class SatellitesCoordinateCalculator:
+    runer = None
+
+    def __init__(self, data, gnssId: GNSS, mode: CalcMode):
+        self.next_func = self.choose_func(data, gnssId, mode)
+
+    def choose_func(self, data, gnssId, mode):
+        match (gnssId, mode):
+            case (GNSS.GPS, CalcMode.alm):
+                return lambda t1, N: calc_gps_alm(data, t1, N)
+            case (GNSS.GPS, CalcMode.eph):
+                return lambda t1, N: calc_gps_eph(data, t1, N)
+            case (GNSS.GLONASS, CalcMode.alm):
+                return lambda t1, N: calc_glo_alm(data, t1, N, hard=True)
+            case (GNSS.GLONASS, CalcMode.eph):
+                self.runer = RK45Solver(*get_glo_eph_simple_func(data))
+                return lambda t1, N: self.runer.get(t1)
+
+    def get(self, t1, N=0):
+        return self.next_func(t1, N)
